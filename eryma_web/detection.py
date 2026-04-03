936d2d8f17d762212564675f82a24dea54d53dde
enzo <enzobaylesocheleau@gmail.com>
2026-04-03 14:14:02 +0200

    test

--- eryma_web/detection.py
+++ eryma_web/detection.py
@@ -1,3 +1,4 @@
+from mailer import send_alert_email
 video_name = "motion_demo_admin_20260329_164103.webm"
 
 add_event(
@@ -5,3 +6,8 @@ add_event(
     description="Détection de mouvement",
     video_filename=video_name
 )
+
+try:
+    send_alert_email(video_name, "video_recorded")
+except Exception as e:
+    print(f"Erreur envoi mail : {e}")
