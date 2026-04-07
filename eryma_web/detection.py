f74987ee767c2301401c49e7270caeb1da100dd9
enzo <enzobaylesocheleau@gmail.com>
2026-04-07 08:50:58 +0200

    commit

--- eryma_web/detection.py
+++ eryma_web/detection.py
@@ -1,12 +1,11 @@
 from mailer import send_alert_email
-from app.services.events import add_event
+from app.services.events import create_event
 
 video_name = "motion_demo_admin_20260329_164103.webm"
 
-add_event(
-    event_type="motion",
-    description="Détection de mouvement",
-    video_filename=video_name
+create_event(
+    kind="motion",
+    video_path=video_name
 )
 
 try:
